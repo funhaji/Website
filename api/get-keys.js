69eba0900eb6d8fa4623c49f179f4d7c6e364001
funhaji <135757344+funhaji@users.noreply.github.com>
2024-12-06 14:01:47 +0330

    Update get-keys.js

--- api/get-keys.js
+++ api/get-keys.js
@@ -1,6 +1,21 @@
-// api/get-keys.js
 export default function handler(req, res) {
-    const keys = ["key1", "key2", "key3"];  // Replace this with your key generation logic
+    const keys = [];
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    // Function to generate a random key
+    function generateKey() {
+        let key = '';
+        for (let i = 0; i < 16; i++) {
+            key += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return key;
+    }
+
+    // Generate 24 keys
+    for (let i = 0; i < 24; i++) {
+        keys.push(generateKey());
+    }
+
+    // Send the keys as a response
     res.status(200).json({ keys });
-  }
-  
+}
